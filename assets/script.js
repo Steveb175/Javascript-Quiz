b79bc456ed013b25d44876844d61fef4a3979a70
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-03-04 13:54:08 -0600

    Added questions and answers array/objects

--- assets/script.js
+++ assets/script.js
@@ -2,19 +2,65 @@
 var startButton = document.getElementById("start-button");
 var time = document.getElementById("timer");
 var instructions = document.getElementById("instructions");
+var multipleChoice = document.getElementById("multiple-choice");
+var timeLeft;
 
-//Objects
-
-var answersSet = {
-  answerOne: "Answer 1",
-  answerTwo: "Answer 2",
-  answerThree: "Answer 3",
-  answerFour: "Answer 4",
-};
+// Questions Object
+var questions = [
+  {
+    question: "Javascript is an _______ language?",
+    answers: [
+      "Object-Oriented",
+      "Object-Based",
+      "Procedural",
+      "None of the above",
+    ],
+    correctAnswer: "Object-Oriented",
+  },
+  {
+    question:
+      "Which of the following keywords is used to define a variable in Javascript?",
+    answers: ["var", "let", "Both A and B", "None of the above"],
+    correctAnswer: "Both A and B",
+  },
+  {
+    question:
+      "Which of the following methods is used to access HTML elements using Javascript?",
+    answers: [
+      "getElementById()",
+      "getElementsByClassName()",
+      "Both A and B",
+      "None of the Above",
+    ],
+    correctAnswer: "getElementById()",
+  },
+  {
+    question:
+      "Upon encountering empty statements, what does the Javascript Interpreter do?",
+    answers: [
+      "Throws an error",
+      "Ignores the statements",
+      "Gives a warning",
+      "None of the above",
+    ],
+    correctAnswer: "Ignores the statements",
+  },
+  {
+    question:
+      "Which of the following methods can be used to display data in some form using Javascript?",
+    answers: [
+      "document.write()",
+      "console.log()",
+      "window.alert()",
+      "All of the above",
+    ],
+    correctAnswer: "All of the above",
+  },
+];
 
 // Timer Function
 function startTimer() {
-  var timeLeft = 10;
+  var timeLeft = 2;
   time.textContent = "Time: " + timeLeft;
   timeCurrent = setInterval(function () {
     if (timeLeft <= 60) {
@@ -31,6 +77,7 @@ function startTimer() {
   }, 1000);
 }
 
+//Start Game Function
 function startGame() {
   startButton.style.display = "none";
   instructions.style.display = "none";
@@ -38,3 +85,6 @@ function startGame() {
 }
 
 //Initial Function
+
+//Test
+console.log(questions);
