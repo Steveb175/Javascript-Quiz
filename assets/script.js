634189b9a1b08e1d0ffb8f0bae2581e003e089d9
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-03-05 15:42:19 -0600

    changed question and answer storage method. Started game functions

--- assets/script.js
+++ assets/script.js
@@ -3,58 +3,61 @@ var startButton = document.getElementById("start-button");
 var time = document.getElementById("timer");
 var instructions = document.getElementById("instructions");
 var multipleChoice = document.getElementById("multiple-choice");
+var questionElement = document.getElementById("question");
+var answerButtonsElement = document.getElementById("answer-button");
 var timeLeft;
-var firstAnswers = questions[0].answers;
+let score = timeLeft;
+var btn = document.getElementsByClassName("btn");
+let currentQuestionIndex;
 
 // Questions Array and Objects
 var questions = [
   {
     question: "Javascript is an _______ language?",
     answers: [
-      "Object-Oriented",
-      "Object-Based",
-      "Procedural",
-      "None of the above",
+      { text: "Object-Oriented", correct: true },
+      { text: "Object-Based", correct: false },
+      { text: "Procedural", correct: false },
+      { text: "None of the above", correct: false },
     ],
-    correctAnswer: "Object-Oriented",
   },
   {
     question:
       "Which of the following keywords is used to define a variable in Javascript?",
-    answers: ["var", "let", "Both A and B", "None of the above"],
-    correctAnswer: "Both A and B",
+
+    answers: [
+      { text: "var", correct: false },
+      { text: "let", correct: false },
+      { text: "Both A and B", correct: true },
+      { text: "None of the above", correct: false },
+    ],
   },
   {
     question:
       "Which of the following methods is used to access HTML elements using Javascript?",
-    answers: [
-      "getElementById()",
-      "getElementsByClassName()",
-      "Both A and B",
-      "None of the Above",
-    ],
+
+    choice1: "getElementById()",
+    choice2: "getElementsByClassName()",
+    choice3: "Both A and B",
+    choice4: "None of the Above",
     correctAnswer: "getElementById()",
   },
   {
     question:
       "Upon encountering empty statements, what does the Javascript Interpreter do?",
-    answers: [
-      "Throws an error",
-      "Ignores the statements",
-      "Gives a warning",
-      "None of the above",
-    ],
+    choice1: "Throws an error",
+    choice2: "Ignores the statements",
+    choice3: "Gives a warning",
+    choice4: "None of the above",
     correctAnswer: "Ignores the statements",
   },
   {
     question:
       "Which of the following methods can be used to display data in some form using Javascript?",
-    answers: [
-      "document.write()",
-      "console.log()",
-      "window.alert()",
-      "All of the above",
-    ],
+    choice1: "document.write()",
+    choice2: "console.log()",
+    choice3: "window.alert()",
+    choice4: "All of the above",
     correctAnswer: "All of the above",
   },
 ];
@@ -69,46 +72,36 @@ function startTimer() {
       time.textContent = "Time: " + timeLeft;
       if (timeLeft === 0) {
         clearInterval(timeCurrent);
+        document.getElementById("question").textContent = "Game Over!";
         time.textContent = "";
         if ((gameOver = true)) {
           time.textContent = "";
+          document.getElementById("question").textContent = "Game Over!";
         }
       }
     }
   }, 1000);
 }
 
-//Start Game Function
+// Start Game Function
 function startGame() {
   startButton.style.display = "none";
   instructions.style.display = "none";
   startTimer();
+  renderNextQuestion();
+  currentQuestionIndex = 0;
 }
 
-//Render questions and answers
-const newLi = document.createElement("li");
-
-function renderQuestions() {
-  // for (let i = 0; i < questions[0].answers.length; i++) {
-  // let firstAnswers = questions[0].answers;
-  // newLi.textContent = firstAnswers;
-  // //   multipleChoice.append(newLi);
-  // }
-  firstAnswers.forEach((item) => {
-    newLi.innerText = item;
-    multipleChoice.appendChild(newLi);
-  });
+// Next Question Function
+function renderNextQuestion() {
+  showQuestion(questions[currentQuestionIndex]);
 }
 
+// Show Question Function
+function showQuestion(question) {
+  questionElement.innerText = questions.question;
+}
+
+// Select Answer Function
+function selectAnswer() {}
 //Initial Function
-
-//Test
-console.log(questions[1].answers);
-console.log(questions[1].answers[0]);
-console.log(questions[1].correctAnswer);
-
-function testText() {
-  const newLi = document.createElement("li");
-  newLi.textContent = questions[1].answers;
-  multipleChoice.append(newLi);
-}
